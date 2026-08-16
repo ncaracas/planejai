d28b06f9cd4c9001a9e5ace075e6d15e757c6793
nelson caracas <contato.ncaracas@gmail.com>
2026-08-16 02:32:27 -0300

    create header, base layout and theme context

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Button } from "./components/shared/Button";
-import { PiggyBank } from "lucide-react";
+import { RootLayout } from "./components/layout/RootLayout";
 
 export const router = createBrowserRouter([
   {
+    element: <RootLayout />,
     children: [
       {
         path: "/",
         element: (
           <>
-            <h1>Formulário de Simulação</h1>,
-            <Button variant="primary" icon={PiggyBank}>
-              Clique aqui
-            </Button>
+            <h1>Formulário de Simulação</h1>            
           </>
         ),
       },
